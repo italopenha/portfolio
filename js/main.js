b745f9b7dc473bb64e61cb792e6bf58d8a760482
Italo <italopenha77@outlook.com>
2024-01-17 13:29:14 -0300

    Melhorias na lógica do botão da barra de navegação

--- js/main.js
+++ js/main.js
@@ -1,6 +1,17 @@
 function toggleMenu() {
     const menu = document.querySelector(".menu-links")
-    const icon = document.querySelector(".hamburger-icon")
+    const hamburgerIcon = document.querySelector(".hamburger-icon")
+    const closeIcon = document.querySelector(".close-icon")
+
+    const isHamburgerIconHidden = window.getComputedStyle(hamburgerIcon).display === "none";
+
+    if (isHamburgerIconHidden) {
+        hamburgerIcon.style.display = "flex"
+        closeIcon.style.display = "none"
+    } else {
+        hamburgerIcon.style.display = "none"
+        closeIcon.style.display = "flex"
+    }
+    
     menu.classList.toggle("open")
-    icon.classList.toggle("open")
 }
